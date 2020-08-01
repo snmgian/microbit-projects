5fa7030a42e1e0a029fdfa1a62ef55772507999d
Gianfranco Zas <snmgian@gmail.com>
2020-08-01 17:14:39 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    basic.pause(6000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    basic.showNumber(randint(1, 6))
+    control.raiseEvent(
+    EventBusSource.MICROBIT_ID_BUTTON_AB,
+    EventBusValue.MICROBIT_BUTTON_EVT_DOWN
+    )
+})
+basic.showIcon(IconNames.Giraffe)
+basic.pause(1000)
+basic.clearScreen()
